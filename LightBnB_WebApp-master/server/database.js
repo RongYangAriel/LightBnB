8b0e2ca6b90d5fc5f1c5c7fa2697a877eb38a86c
RongYangAriel <yang.ariel@hotmail.com>
2020-11-26 17:56:55 -0500

    update getAllReservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -79,7 +79,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(
+    `SELECT reservations.*, properties.*, AVG(property_reviews.rating) 
+    FROM properties
+    JOIN reservations ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE $1= 1 AND reservations.end_date < now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date DESC
+    LIMIT 10;`, [guest_id]
+  ).then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
